95a175aeb0cb53bf30ab33d2811726276043d8e1
z <z@v0.vc>
2020-05-13 01:49:47 +0200

    :sparkles: improved memory-based storage

--- internal/pkg/store/memory.go
+++ internal/pkg/store/memory.go
@@ -1,14 +1,17 @@
 package store
 
-import "sync"
+import (
+	"sync"
+	"time"
+)
 
 type MemoryDB struct {
-	storage map[string]bool
+	storage map[string]time.Time
 	mux     *sync.RWMutex
 }
 
 func (s *MemoryDB) New() (Store, error) {
-	s.storage = make(map[string]bool)
+	s.storage = make(map[string]time.Time)
 	s.mux = &sync.RWMutex{}
 
 	return s, nil
@@ -16,20 +19,34 @@ func (s *MemoryDB) New() (Store, error) {
 
 func (s *MemoryDB) PutKey(key string) error {
 	s.mux.Lock()
-	s.storage[key] = true
+	s.storage[key] = time.Now()
 	s.mux.Unlock()
 
 	return nil
 }
 
-func (s MemoryDB) KeyExists(key string) (bool, error) {
+func (s *MemoryDB) KeyExists(key string) (bool, error) {
 	s.mux.RLock()
 	_, ok := s.storage[key]
 	s.mux.RUnlock()
 
 	if ok {
+		s.mux.Lock()
+		s.storage[key] = time.Now()
+		s.mux.Unlock()
+
 		return true, nil
 	}
 
 	return false, nil
+}
+
+func (s *MemoryDB) CleanupBefore(before time.Time) {
+	s.mux.Lock()
+	for key, lastSeenAt := range s.storage {
+		if lastSeenAt.Before(before) {
+			delete(s.storage, key)
+		}
+	}
+	s.mux.Unlock()
 }
